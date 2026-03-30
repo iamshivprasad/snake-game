9521b370916b57baec45687e52abb914a58502d5
Shiv <42866319+iamshivprasad@users.noreply.github.com>
2026-03-30 11:20:03 +0100

    Added game status and game over conditions.

--- App/Source/main.cpp
+++ App/Source/main.cpp
@@ -1,22 +1,75 @@
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3/SDL_surface.h>
 #include <iostream>
 #include <list>
 
-#define MAX_WIDTH 800
-#define MAX_HEIGHT 600
+#define MAX_PLAYFIELD_WIDTH 800
+#define MAX_PLAYFIELD_HEIGHT 600
+#define INFO_PANEL_HEIGHT 200
+#define INFO_PANEL_WIDTH 800
+
+enum GameStatus
+{
+	Playing,
+	GameOver
+};
 
 auto draw_grid(SDL_Renderer* renderer, float snake_cell_size) -> void
 {
 	SDL_SetRenderDrawColor(renderer, 40, 40, 40, 255);
-	for (float x = 0; x < 800; x += snake_cell_size)
+	for (float x = 0; x < MAX_PLAYFIELD_WIDTH; x += snake_cell_size)
 	{
-		SDL_RenderLine(renderer, x, 0, x, 600);
+		SDL_RenderLine(renderer, x, 0, x, MAX_PLAYFIELD_HEIGHT);
 	}
 
-	for (float y = 0; y < 600; y += snake_cell_size)
+	for (float y = 0; y < MAX_PLAYFIELD_HEIGHT; y += snake_cell_size)
 	{
-		SDL_RenderLine(renderer, 0, y, 800, y);
+		SDL_RenderLine(renderer, 0, y, MAX_PLAYFIELD_WIDTH, y);
 	}
+}
+
+auto draw_info_panel(SDL_Renderer* renderer) -> void
+{
+	const SDL_FRect info_panel_rect{
+	.x = 0,
+	.y = MAX_PLAYFIELD_HEIGHT,
+	.w = INFO_PANEL_WIDTH,
+	.h = INFO_PANEL_HEIGHT
+	};
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderRect(renderer, &info_panel_rect);
+
+}
+
+auto render_text(SDL_Renderer* renderer, TTF_Font* font, const std::string& text, float x, float y) -> void
+{
+	if (text.empty())
+	{
+		return;
+	}
+
+	SDL_Color colour = { 255, 255, 255, 255 };
+
+	SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), 0, colour);
+	if (!surface)
+	{
+		return;
+	}
+
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	if (texture) {
+		SDL_FRect dst_rect = { x, y, (float)surface->w, (float)surface->h };
+		if (!SDL_RenderTexture(renderer, texture, nullptr, &dst_rect))
+		{
+			SDL_Log("TTF_RenderText_Blended failed: %s", SDL_GetError());
+			return;
+		}
+		SDL_DestroyTexture(texture);
+	}
+
+	SDL_DestroySurface(surface);
 }
 
 int main(int, char**)
@@ -27,7 +80,7 @@ int main(int, char**)
 		return 1;
 	}
 
-	SDL_Window* window = SDL_CreateWindow("Snake Game", 800, 600, 0);
+	SDL_Window* window = SDL_CreateWindow("Snake Game", 800, 800, 0);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
 
 	bool running = true;
@@ -37,16 +90,44 @@ int main(int, char**)
 	float snake_cell_size = 10;
 
 	std::list<SDL_FRect> snake_body;
-	for(auto i = 0; i < snake_size - 1; ++i)
+	for (auto i = 0; i < snake_size - 1; ++i)
 	{
-		snake_body.push_back({ MAX_WIDTH / 2 - (i * snake_cell_size), MAX_HEIGHT / 2, snake_cell_size, snake_cell_size });
+		snake_body.push_back({ MAX_PLAYFIELD_WIDTH / 2 - (i * snake_cell_size), MAX_PLAYFIELD_HEIGHT / 2, snake_cell_size, snake_cell_size });
 	}
-
 
 	auto current_direction = SDLK_RIGHT;
 	auto previous_direction = current_direction;
+
+	if (!TTF_Init())
+	{
+		SDL_Log("TTF_Init failed: %s", SDL_GetError());
+		return 1;
+	}
+
+	TTF_Font* font = TTF_OpenFont("C:\\Windows\\Fonts\\arial.ttf", 24);
+	if (!font)
+	{
+		SDL_Log("Could not open font: %s", SDL_GetError());
+		return 1;
+	}
+
+	GameStatus game_status = GameStatus::Playing;
 	while (running)
 	{
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		// Draw grid for play area
+		draw_grid(renderer, snake_cell_size);
+		draw_info_panel(renderer);
+
+		if (game_status == GameStatus::GameOver)
+		{
+			render_text(renderer, font, "Game Over! You hit the wall.", 200, 300);
+			SDL_RenderPresent(renderer);
+			continue;
+		}
+
 		while (SDL_PollEvent(&e)) {
 
 			switch (e.type)
@@ -55,7 +136,7 @@ int main(int, char**)
 				switch (e.key.key)
 				{
 				case SDLK_RIGHT:
-					if(previous_direction != SDLK_LEFT)
+					if (previous_direction != SDLK_LEFT)
 					{
 						previous_direction = current_direction;
 						current_direction = SDLK_RIGHT;
@@ -67,7 +148,7 @@ int main(int, char**)
 
 				case SDLK_LEFT:
 
-					if(previous_direction != SDLK_RIGHT)
+					if (previous_direction != SDLK_RIGHT)
 					{
 						previous_direction = current_direction;
 						current_direction = SDLK_LEFT;
@@ -109,22 +190,28 @@ int main(int, char**)
 				break;
 			}
 
-
 		}
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		// Draw grid for play area
-		draw_grid(renderer, snake_cell_size);
-
-		// Draw snake body
-		SDL_SetRenderDrawColor(renderer, 0, 200, 0, 255);
-		for (const auto& segment : snake_body)
+		if (snake_body.front().x < 0 ||
+			snake_body.front().x + snake_cell_size > MAX_PLAYFIELD_WIDTH ||
+			snake_body.front().y < 0 ||
+			snake_body.front().y + snake_cell_size > MAX_PLAYFIELD_HEIGHT)
 		{
-			SDL_RenderFillRect(renderer, &segment);
+			//render_text(renderer, "Game Over! You hit the wall.", 200, 300);
+			// running = false;
+			std::cout << "Game Over! You hit the wall.\n";
+			game_status = GameStatus::GameOver;
 		}
+		else
+		{
+			// Draw snake body
+			SDL_SetRenderDrawColor(renderer, 0, 200, 0, 255);
+			for (const auto& segment : snake_body)
+			{
+				SDL_RenderFillRect(renderer, &segment);
+			}
 
+		}
 		SDL_RenderPresent(renderer);
 	}
 
--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,6 +1,8 @@
 # Dependencies.cmake
 
 find_package(SDL3 CONFIG REQUIRED)
+find_package(SDL3_ttf REQUIRED)
+
 
 # Create an interface library to bundle all external deps
 add_library(ExternalLibs INTERFACE)
@@ -8,4 +10,5 @@ add_library(ExternalLibs INTERFACE)
 target_link_libraries(ExternalLibs
     INTERFACE
         SDL3::SDL3
-)
+        SDL3_ttf::SDL3_ttf
+)        
